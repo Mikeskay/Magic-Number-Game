d1102a9108ca9e81111beb9ff2de50fdd27c1952
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 22:38:30 +0000

    Create MagicNumberGameTest.java

---
+++ test/MagicNumberGameTest.java
@@ -0,0 +1,15 @@
+package magicnumbergame.test;
+
+import magicnumbergame.base.engine.MagicNumberGame;
+import magicnumbergame.base.display.MagicNumberGameDisplay;
+
+public class MagicNumberGameTest {
+
+    public static void main(String[] args) {
+
+        MagicNumberGameDisplay test = new MagicNumberGameDisplay();
+        
+    }
+
+
+}
